78256f516c5af9964bf6a93e2c332bb1805fa95c
ingridsfonoyd@gmail.com <ingridsfonoyd@gmail.com>
2022-10-04 19:36:45 -0500

    codeSession2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import { initDB } from "./db/index.js";
+import express from "express";
+import { initDB } from "./db/index.js"
 
-initDB().then(() => {
-  console.log("DB Created :)");
+const Api = express();
+
+Api.use(express.json());
+Api.use(express.urlencoded({ extended: false }));
+
+Api.get("/test", ({ request, response }) => {
+  response.send({ message: "It works :)" });
+});
+
+Api.listen(3000, () => {
+  console.log("API IS RUNNING :)\n");
+  initDB().then(() => {
+    console.log("DB IS READY :)");
+  });
 });
