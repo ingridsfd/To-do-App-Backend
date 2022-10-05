dec08168913079d4f5ddd7608158d4bf68517776
ingridsfonoyd@gmail.com <ingridsfonoyd@gmail.com>
2022-10-05 13:47:46 -0500

    apiWorks

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const Api = express();
 Api.use(express.json());
 Api.use(express.urlencoded({ extended: false }));
 
-Api.get("/test", ({ request, response }) => {
-  response.send({ message: "It works :)" });
+Api.get("/test", ({ req, res }) => {
+  res.send({ message: "It works" });
 });
 
 Api.listen(3000, () => {
-  console.log("API IS RUNNING :)\n");
+  console.log("API IS RUNNING OK\n");
   initDB().then(() => {
-    console.log("DB IS READY :)");
+    console.log("DB IS READY");
   });
 });
