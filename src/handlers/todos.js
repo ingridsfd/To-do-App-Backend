041f9008d393a819c55a45db37200ca5b81943ce
ingridsfonoyd@gmail.com <ingridsfonoyd@gmail.com>
2022-10-07 15:09:25 -0500

    challengeCommits

---
+++ src/handlers/todos.js
@@ -0,0 +1,107 @@
+import Express, { response } from "express";
+import { getDBHandler } from "../db/index.js";
+
+const ToDosRequestHandler = Express.Router();
+
+ToDosRequestHandler.post("/to-dos", async (request, response) => {
+  try {
+    const { title, description, isDone: is_done } = request.body;
+    const dbHandler = await getDBHandler();
+
+    const newTodo = await dbHandler.run(`
+        INSERT INTO todos (title, description, is_done)
+        VALUES (
+            '${title}',
+            '${description}',
+            ${is_done}
+        )
+    `);
+
+    await dbHandler.close();
+
+    response.send({ newTodo: { title, description, is_done, ...newTodo } });
+  } catch (error) {
+    response.status(500).send({
+      error: `Something went wrong when trying to create a new to do`,
+      errorInfo: error.message,
+    });
+  }
+});
+
+ToDosRequestHandler.get("/to-dos", async (request, response) => {
+  try {
+    const dbHandler = await getDBHandler();
+
+    const todos = await dbHandler.all("SELECT * FROM todos");
+    await dbHandler.close();
+
+    if (!todos || !todos.length) {
+      return response.status(404).send({ message: "To Dos Not Found" });
+    }
+
+    response.send({ todos });
+  } catch (error) {
+    response.status(500).send({
+      error: `Something went wrong when trying to get the to dos`,
+      errorInfo: error.message,
+    });
+  }
+});
+
+ToDosRequestHandler.delete("/to-dos/:id", async (request, response) => { //la ponemos asíncrona para que retorne algo con el tiempo
+  try {
+    const todoId = request.params.id;
+    const dbHandler = await getDBHandler();
+
+    const deletedTodo = await dbHandler.run(
+      "DELETE FROM todos WHERE id = ?",
+      todoId
+    );
+
+    await dbHandler.close();
+
+    response.send({ todoRemoved: { ...deletedTodo } });
+  } catch (error) {
+    response.status(500).send({
+      error: `Something went wrong when trying to delete the to do`,
+      errorInfo: error.message,
+    });
+  }
+});
+
+//Challenge: realizar el update
+ToDosRequestHandler.put("/to-dos", async (request, response) => {
+    //redacta el try - catch
+    try { // ¿Queremos actualizar todos los datos? Sí
+    //Hay que indicarle de dónde a dónde en nuestra base de datos. Usa la referencia del post
+        const { title, description, isDone: is_done } = request.body;
+        const dbHandler = await getDBHandler();
+
+        const newTodo = await dbHandler.run(`
+            INSERT INTO todos (title, description, is_done)
+            VALUES (
+                '${title}',
+                '${description}',
+                ${is_done}
+            )
+        `);
+
+        await dbHandler.close();
+    //Ahora, dile qué necesitamos a hacer con los datos: actualizarlos
+    //¿cómo lo redactarías? primero que si el contenido de todos esos params deja de ser igual a sí mismo, que retorne un post vacío
+        if ({ title, description, isDone: is_done } !== { title, description, isDone: is_done }) {
+            //return nuevo post vacío
+        } else {
+            return response.status(404).send({ message: "To Dos Not Found" });
+        };
+
+    response.send({ todos });
+    } catch (error) {
+        response.status(500).send({
+            error: `Something went wrong when trying to get the to dos`,
+            errorInfo: error.message,
+        });
+    }
+});
+    
+export default ToDosRequestHandler; 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+import cors from "cors";
 import express from "express";
-import { initDB } from "./db/index.js"
+import { initDB } from "./db/index.js";
+import ToDosRequestHandler from "./handlers/todos.js";
 
 const Api = express();
 
 Api.use(express.json());
 Api.use(express.urlencoded({ extended: false }));
 
-Api.get("/test", ({ req, res }) => {
-  res.send({ message: "It works" });
-});
+Api.use(cors());
+Api.use("/v1", ToDosRequestHandler);
 
 Api.listen(3000, () => {
-  console.log("API IS RUNNING OK\n");
+  console.log("API IS RUNNING");
   initDB().then(() => {
     console.log("DB IS READY");
   });
